bdbac524567f069aa0d8c42eddaf8843400f2a99
ponchix <ponchorc28@gmail.com>
2021-04-29 16:54:44 -0500

    Subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { IAComponent } from './ia/ia.component';
 
+const rutas: Routes = [
+  { path: 'home', component: HomeComponent, pathMatch:'full'  },
+  { path: 'reconocimiento', component: IAComponent  },
+  { path: '', redirectTo: 'home', pathMatch:'full'  },
+
+  
+  
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IAComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
